036164eaab10ac54e7f97487b4f7c5afefb40df0
Born <jing23501@qq.com>
2019-06-23 01:32:57 +0800

    2

--- workspace-gateway/src/main/java/com/aliware/tianchi/stats/DataCollector.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/stats/DataCollector.java
@@ -10,7 +10,7 @@ public class DataCollector {
     private AtomicInteger activeRequests = new AtomicInteger(0);
     private AtomicInteger failedRequests = new AtomicInteger(0);
     private BucketRate bucketRate = new BucketRate(3000, 100, 50);
-    private QPSRate qps = new QPSRate(3000,1000,3);
+    private QPSRate qps = new QPSRate(3000,500,3);
     private DistributionRate distributionRate = new DistributionRate(3000, 100);
 
 
