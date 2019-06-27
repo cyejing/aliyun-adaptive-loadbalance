35326cda1d0bcd3e3840cc43f45e3ba0049556d9
Born <jing23501@qq.com>
2019-06-27 14:17:38 +0800

    1

--- workspace-gateway/src/main/java/com/aliware/tianchi/stats/DataCollector.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/stats/DataCollector.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class DataCollector {
 
     public static final double ALPHA = 0.962;
-    public static final double GAMMA = 1;
+    public static final double GAMMA = 1.05;
 
 
     private volatile int bucket = 1000;
@@ -57,7 +57,7 @@ public class DataCollector {
 
     public int getWeight() {
         double mean = distributionRate.getMean();
-        double r = Math.pow(1000 / mean, GAMMA) * (Math.pow(bucket, ALPHA)+getActive());
+        double r = Math.pow(1000 / mean, GAMMA) * (Math.pow(bucket, ALPHA) + getActive());
         return new Double(r).intValue();
     }
 
