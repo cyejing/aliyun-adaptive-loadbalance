e9b97d7e3b4522eae18e135864c6dd21d64794ae
born <jing23501@qq.com>
2019-06-28 19:49:41 +0800

    1

--- workspace-gateway/src/main/java/com/aliware/tianchi/stats/DataCollector.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/stats/DataCollector.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class DataCollector {
 
     public static final double ALPHA = 0.962;
-    public static final double GAMMA = 1.05;
+    public static final double GAMMA = 1.10;
 
 
     private volatile int bucket = 1000;
