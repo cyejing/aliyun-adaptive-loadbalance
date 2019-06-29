aa3f86720811f0f4d062ee9de37769e5190d0104
Born <jing23501@qq.com>
2019-06-29 15:03:23 +0800

    1

--- workspace-gateway/src/main/java/com/aliware/tianchi/stats/DistributionRate.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/stats/DistributionRate.java
@@ -11,7 +11,6 @@ public class DistributionRate {
     private int size;
     private double[] requestRTTs;
     private double[] currs;
-    private volatile double curr = 0;
     private AtomicInteger index = new AtomicInteger(0);
 
     private long delayThreshold;
@@ -60,9 +59,6 @@ public class DistributionRate {
             this.startTime = System.currentTimeMillis();
         } else if (i % (size) == 0) {
             this.currs[(i / size) % 10] = (1000D / (now - startTime) * (size)) / (1000D / mean);
-            if (((i / size) % 10) == 0) {
-                this.currs[(i / size) % 10] += 20;
-            }
             this.startTime = now;
         }
 
@@ -85,7 +81,7 @@ public class DistributionRate {
             total = s;
         }
         if (s == 0) {
-            return 0D;
+            return 100D;
         }
         return totalCurr / total;
     }
