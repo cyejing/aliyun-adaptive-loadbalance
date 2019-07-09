6488a5ac8ffa7218af65b39e08dacfa4365ffb6e
Born <jing23501@qq.com>
2019-07-09 20:07:44 +0800

    4

--- workspace-gateway/src/main/java/com/aliware/tianchi/stats/ThroughputRate.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/stats/ThroughputRate.java
@@ -1,13 +1,8 @@
 package com.aliware.tianchi.stats;
 
-import static com.aliware.tianchi.stats.DataCollector.ALPHA;
-
 import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Born
@@ -19,6 +14,7 @@ public class ThroughputRate {
     private volatile double throughputRate;
     private volatile double weight;
     private volatile AtomicInteger rise = new AtomicInteger(0);
+    private volatile AtomicBoolean calc = new AtomicBoolean(false);
 
     private long interval;
     private volatile long threshold;
@@ -49,8 +45,10 @@ public class ThroughputRate {
         long now = System.currentTimeMillis();
         long t = this.threshold;
         if (now > t) {
-            synchronized (this) {
+
+            if (calc.compareAndSet(false, true)) {
                 if (now > threshold) {
+                    System.out.println(LocalDateTime.now().toString() + " 时间变化weight" + this.weight);
                     double oWeight = this.weight;
                     double nWeight = i * (1000D / (now - t + interval));
                     this.throughputRate = nWeight;
@@ -60,7 +58,7 @@ public class ThroughputRate {
                     if (nWeight > oWeight || devWeight > 1200) {
                         this.weight = nWeight;
                         this.rise.set(1);
-                    }else{
+                    } else {
                         this.weight = oWeight;
                         this.rise.decrementAndGet();
                     }
@@ -68,7 +66,9 @@ public class ThroughputRate {
                     this.throughput.set(0);
                     this.threshold = now + interval;
                 }
+                calc.compareAndSet(false, true);
             }
+
         }
     }
 
