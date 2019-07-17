52c0799b2673f3240970722c28dc71013f0f65cf
Born <jing23501@qq.com>
2019-07-17 14:51:27 +0800

    2

--- workspace-gateway/src/main/java/com/aliware/tianchi/stats/DataCollector.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/stats/DataCollector.java
@@ -11,7 +11,7 @@ public class DataCollector {
 
 
     public static final double ALPHA = 0.775;
-    public static final double BETA = 0.0125;
+    public static final double BETA = 0.0185;
     public static final double GAMMA = 0.25;
     public static final double NEUTRON = 2.10;
 
